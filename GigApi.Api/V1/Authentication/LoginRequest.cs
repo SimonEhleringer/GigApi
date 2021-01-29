c68df981bac8dc4b0f38e877a5b456f7118b3396
Simon Ehleringer <simon@ehleringer.de>
2021-01-29 20:15:49 +0100

    Change validation in LoginRequest

--- GigApi.Api/V1/Authentication/LoginRequest.cs
+++ GigApi.Api/V1/Authentication/LoginRequest.cs
@@ -9,14 +9,9 @@ namespace GigApi.Api.V1.Authentication
     public class LoginRequest
     {
         [Required(ErrorMessage = "Es wurde keine E-Mail Adresse übermittelt.")]
-        [EmailAddress(ErrorMessage = "Die E-Mail Adresse ist ungültig.")]
-        [MaxLength(256, ErrorMessage = "Die E-Mail Adresse darf höchstens 256 Zeichen lang sein.")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Es wurde kein Passwort übermittelt.")]
-        [MaxLength(256, ErrorMessage = "Das Passwort darf höchstens 256 Zeichen lang sein.")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$", 
-            ErrorMessage = "Das Password muss mindestens sechs Zeichen lang sein, einen Groß- und Kleinbuchstaben sowie eine Zahl und ein Sonderzeichen enthalten.")]
         public string Password { get; set; }
     }
 }
